ea4746c482d99583e9fa437480e87dd3343fb1be
SrivalliGottipati <121477373+SrivalliGottipati@users.noreply.github.com>
2023-08-05 11:57:54 +0530

    import java.util.Scanner; public class Kb{     public static void main(String[] args){     Scanner sc=new Scanner(System.in);     int a,b,c,d;     a=sc.nextInt();     b=sc.nextInt();     c=sc.nextInt();     d=(2*a*b*c*512)/1024;     System.out.println(d+" KB"); } }

---
+++ Capacity.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Kb{
+    public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int a,b,c,d;
+    a=sc.nextInt();
+    b=sc.nextInt();
+    c=sc.nextInt();
+    d=(2*a*b*c*512)/1024;
+    System.out.println(d+" KB");
+}
+}
