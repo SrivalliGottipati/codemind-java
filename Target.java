b02b5f478bfccd219eedc2ce8adb71706e713a97
SrivalliGottipati <121477373+SrivalliGottipati@users.noreply.github.com>
2023-08-17 11:08:10 +0530

    import java.util.Scanner; public class Tar{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int p1=sc.nextInt();         int p2=sc.nextInt();         int p3=sc.nextInt();         int p4=sc.nextInt();         if(p1<10 || p2<10 || p3<10 || p4<10){             System.out.println("NO");         }         else{             System.out.println("YES");         }     } }

---
+++ Target.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Tar{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int p1=sc.nextInt();
+        int p2=sc.nextInt();
+        int p3=sc.nextInt();
+        int p4=sc.nextInt();
+        if(p1<10 || p2<10 || p3<10 || p4<10){
+            System.out.println("NO");
+        }
+        else{
+            System.out.println("YES");
+        }
+    }
+}
