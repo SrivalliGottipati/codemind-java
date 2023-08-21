b21020434bed4e520b0a33392157c04e4a2101f5
SrivalliGottipati <121477373+SrivalliGottipati@users.noreply.github.com>
2023-08-21 13:36:15 +0530

    import java.util.Scanner; public class Mul{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         for(int i=1;i<=a;i++){             System.out.print(3*i+" ");         }     } }

---
+++ First_N_multiples_of_3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Mul{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        for(int i=1;i<=a;i++){
+            System.out.print(3*i+" ");
+        }
+    }
+}
