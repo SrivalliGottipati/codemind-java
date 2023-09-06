52a31f633d57970bb1d000736af913ae0f9bebcd
SrivalliGottipati <121477373+SrivalliGottipati@users.noreply.github.com>
2023-09-06 21:40:26 +0530

    import java.util.Scanner; public class Profit{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int cp=sc.nextInt();         int sp=sc.nextInt();         if(cp>sp){             System.out.println("Loss");         }         else if(cp<sp){             System.out.println("Profit");         }         else{             System.out.println("No profit and No loss");         }     } }

--- Profit_or_Loss.java
+++ Profit_or_Loss.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
-public class Pro{
+public class Profit{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        if(a<b){
-            System.out.println("Profit");
-        }
-        else if(a>b){
+        int cp=sc.nextInt();
+        int sp=sc.nextInt();
+        if(cp>sp){
             System.out.println("Loss");
+        }
+        else if(cp<sp){
+            System.out.println("Profit");
         }
         else{
             System.out.println("No profit and No loss");
