3840bf81fa0a8ab25fc9564fe0fd9242d16a01c5
SrivalliGottipati <121477373+SrivalliGottipati@users.noreply.github.com>
2023-09-06 21:37:09 +0530

    import java.util.Scanner; public class Even{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         if(n%2==0){             System.out.println("Even");         }         else{             System.out.println("Odd");         }     } }

--- Even_or_Odd.java
+++ Even_or_Odd.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
-public class Eo{
+public class Even{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        if(a%2==0){
+        int n=sc.nextInt();
+        if(n%2==0){
             System.out.println("Even");
         }
-        else {
+        else{
             System.out.println("Odd");
         }
     }
